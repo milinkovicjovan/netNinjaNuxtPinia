72df444f59827d5dfae431c7d19813ecc656acac
milinkovicjovan <milinkovicjovanbusiness@gmail.com>
2022-12-06 22:30:55 +0100

    totalPriceAndDeletingItems

--- pages/cart.vue
+++ pages/cart.vue
@@ -8,6 +8,17 @@
         <img :src="product.img" :alt="product.title" />
         <p class="text-white">{{ product.title }}</p>
         <p class="text-white">{{ product.price * product.quantity }} coins.</p>
+
+        <!--delete-->
+        <button class="ml-auto" @click="cartStore.deleteFromCart(product)">
+          <span class="material-icons-outlined text-white">delete</span>
+        </button>
+      </div>
+
+      <!-- cart total-->
+      <div class="text-right pt-4 mt-4 border-t-2">
+        <span class="text-white">Total amount to pay:</span>
+        <span class="text-secondary">{{ cartStore.cartTotal }}</span>
       </div>
     </div>
 
--- stores/cartStore.js
+++ stores/cartStore.js
@@ -4,11 +4,28 @@ export const useCartStore = defineStore("cart", {
   state: () => ({
     cart: [],
   }),
+  getters: {
+    cartTotal() {
+      return this.cart.reduce((total, item) => {
+        return total + item.price * item.quantity;
+      }, 0);
+    },
+  },
   actions: {
     async getCart() {
       const data = await $fetch("http://localhost:4000/cart");
       this.cart = data;
       console.log(this.cart);
+    },
+    async deleteFromCart(product) {
+      this.cart = this.cart.filter((p) => {
+        return p.id !== product.id;
+      });
+
+      // make delete request
+      await $fetch("http://localhost:4000/cart/" + product.id, {
+        method: "delete",
+      });
     },
   },
 });
